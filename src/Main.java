a6d090df407b335c68f3f847b7332d912593a141
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 09:09:44 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Music smile = new Music("Die with a smile", "Ester Junta", "pop");
-        Music judas = new Music("Judas", "lady Gaga", "pop");
-        Music after = new Music("After hours","the Weekend", "pop");
+        Music smile = new Music("Die with a smile", "Ester Junta", "pop", 30);
+        Music judas = new Music("Judas", "lady Gaga", "pop", 40);
+        Music after = new Music("After hours","the Weekend", "pop", 45);
+        Music standing = new Music("I'm still standing", "Elton John", "pop", 60);
+        Music lights = new Music("Blinding lights", "the Weekend", "pop", 50);
+        Music poker = new Music("Poker Face","lady Gaga", "pop", 40);
 
 
         User Kutya = new User("Ermekov Kutmansher", "POP", 800);
@@ -10,7 +13,26 @@ public class Main {
 //        Kutya.getInfo();
 //        Dastan.getInfo();
         Kutya.getBalance();
-        Kutya.buyMusic(night_call);
+        Kutya.buyMusic(judas);
         Kutya.getBalance();
+
+
+        Kutya.getBalance();
+        Kutya.buyMusic(judas);
+        Kutya.buyMusic(standing);
+        Kutya.buyMusic(judas);
+        Kutya.buyMusic(standing);
+        Kutya.buyMusic(lights);
+        Kutya.buyMusic(poker);
+        Kutya.getBalance();
+        Dastan.getBalance();
+        Dastan.buyMusic(smile);
+        Dastan.buyMusic(judas);
+        Dastan.buyMusic(poker);
+        Dastan.buyMusic(poker);
+        Dastan.buyMusic(poker);
+        Dastan.getBalance();
+        Kutya.getPlaylist();
+        Dastan.getPlaylist();
     }
 }
