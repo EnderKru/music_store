b32bdd8f551b9b528c2206b65078422638fcf24e
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 10:26:39 +0600

    Update Music.java

--- src/Music.java
+++ src/Music.java
@@ -1,17 +1,27 @@
+import java.util.Vector;
+import java.awt.Desktop;
+import java.net.URI;
+
 public class Music {
     String name;
     String author;
     String genre;
     double price;
     Integer views;
+    Vector<Integer> rates;
+    String url;
 
-    public Music(String name, String author, String genre, double price)){
+    public Music(String name, String author, String genre, String url,  double price)){
         this.name = name;
         this.author = author;
         this.genre = genre;
+        this.url = url;
         this.price = price;
         this.views = 0;
+        this.rates = new Vector<Integer>();
     }
+    
+    
     public void get_info(){
         System.out.println(this.name + " by " + this.author + ". genre: " + this.genre + ". Costs " +this.price);
     }
@@ -22,6 +32,32 @@ public class Music {
 
     public void getViews(){
         System.out.println(this.name + " was listened " + this.views + " times");
+    }
+
+        public void getRate(){
+        System.out.print(this.name + " got: ");
+        for(int i=0; i < Math.round(this.getAverage()); i++){
+            System.out.print("⭐");
+        }
+    }
+    public double getAverage(){
+        double result = 0;
+        for(int i=0; i < this.rates.size(); i++){
+            result += this.rates.get(i);
+        }
+        return result / this.rates.size();
+    }
+    
+    public void openVideo(){
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI(this.url));
+            } else {
+                System.out.println("Открытие браузера не поддерживается.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
