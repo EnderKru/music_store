c738a1bfd211fb43944c4a54b7317ffda0241ca2
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 09:20:58 +0600

    Update Music.java

--- src/Music.java
+++ src/Music.java
@@ -3,12 +3,14 @@ public class Music {
     String author;
     String genre;
     double price;
+    Integer views;
 
     public Music(String name, String author, String genre, double price)){
         this.name = name;
         this.author = author;
         this.genre = genre;
         this.price = price;
+        this.views = 0;
     }
     public void get_info(){
         System.out.println(this.name + " by " + this.author + ". genre: " + this.genre + ". Costs " +this.price);
@@ -16,6 +18,10 @@ public class Music {
 
     public String toString(){
         return this.name + " by " + this.author ;
+    }
+
+    public void getViews(){
+        System.out.println(this.name + " was listened " + this.views + " times");
     }
 }
 
