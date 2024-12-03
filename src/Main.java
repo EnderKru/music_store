a4cfacc574142bcb7f060e3b7fc7ed1bcbd75435
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 08:45:48 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Music smile = new Music("Die with a smile", "Ester Junta", "pop");
+        Music judas = new Music("Judas", "lady Gaga", "pop");
+        Music after = new Music("After hours","the Weekend", "pop");
+        System.out.println(smile.get_info());
+        System.out.println(judas.get_info());
+        System.out.println(after.get_info());
     }
 }
