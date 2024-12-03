ecfe765be3a229da655f743f77fe7ae9dfe34c72
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 08:46:40 +0600

    Update Music.java

--- src/Music.java
+++ src/Music.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Music {
+    String name;
+    String author;
+    String genre;
+
+    public Music(String name, String author, String genre){
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+    }
+    public String get_info(){
+        return this.name + " by " + this.author + ". genre: " + this.genre;
+    }
 }
