ce1dfa5e136c6260048ce1cb050341c7f5466cf8
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-03 08:56:11 +0600

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+import java.util.Vector;
 public class User {
+    String name;
+    String favorite_genre;
+    double money;
+    Vector<Music> playList;
+    public User(String name, String favorite_genre, double money){
+        this.name = name;
+        this.favorite_genre = favorite_genre;
+        this.money = money;
+        this.playList = new Vector<Music>();;
+    }
+    public void getInfo(){
+        System.out.println(this.name + " likes " + this.favorite_genre); ;
+    }
+    public void getBalance(){
+         System.out.println(this.name + " has " + this.money) ;
+    }
+    public void buyMusic(Music music){
+        this.playList.add(music);
+        this.money -= music.price;
+        System.out.println(this.name + " bought " + music + " for " + music.price);
+    }
 }
