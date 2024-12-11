b71c09a2637b1946dfdd6d23f02ea219e50cded3
EnderKru <144217951+EnderKru@users.noreply.github.com>
2024-12-11 21:56:40 +0600

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,8 +1,10 @@
 import java.util.Vector;
+
 public class User {
     String name;
     String favorite_genre;
     double money;
+
     Vector<Music> playList;
     public User(String name, String favorite_genre, double money){
         this.name = name;
@@ -10,30 +12,33 @@ public class User {
         this.money = money;
         this.playList = new Vector<Music>();;
     }
+
     public void getInfo(){
         System.out.println(this.name + " likes " + this.favorite_genre); ;
     }
+
     public void getBalance(){
-         System.out.println(this.name + " has " + this.money) ;
+        System.out.println(this.name + " has " + this.money) ;
     }
+
     public void buyMusic(Music music, Integer rate){
-        this.playList.add(music);
-        this.money -= music.price;
-        System.out.println(this.name + " bought " + music + " for " + music.price);
         if (this.musicExists(music)){
             System.out.println(this.name + " already has bought " + music);
         }else if(this.money < music.price){
             System.out.println(this.name + " cab't buy " + music + " due to the financial issues ");
         }else if(rate > 5 || rate < 1){
             System.out.println("rates can be only from 1 to 5");
-        }else{
+        }
+        else{
             this.playList.add(music);
             this.money -= music.price;
+            music.downloads += 1;
             music.rates.add(rate);
             System.out.println(this.name + " bought " + music + " for " + music.price);
         }
+
     }
-    
+
     public boolean musicExists(Music music){
         for(int i=0; i<this.playList.size(); i++){
             if(this.playList.get(i).equals(music)){
@@ -42,18 +47,20 @@ public class User {
         }
         return false;
     }
-    
+
     public void getPlaylist(){
-        System.out.println(this.name + " has these songs: \n");
+        System.out.println("\n\n" + this.name + " has these songs: \n");
         for(int i=0; i<this.playList.size(); i++){
             System.out.println(this.playList.get(i));
-            }
-                System.out.println("\n\n");
+        }
+        System.out.println("\n\n");
     }
+
     public void cashIn(double cash){
         this.money += cash;
         System.out.println(this.name + " cash in " + cash + " dollars");
     }
+
     public void listenMusic(Music music){
         if (this.musicExists(music)){
             System.out.println(this.name + " is listening " + music + "...♫⋆｡♪ ₊˚♬ ﾟ.");
@@ -63,5 +70,5 @@ public class User {
             System.out.println(this.name + " can't listen  " + music);
         }
     }
-    
+
 }
